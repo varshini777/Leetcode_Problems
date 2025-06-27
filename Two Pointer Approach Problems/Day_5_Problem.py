7a5df43cbdd107ed246312f969a4864b1885881d
Varshini Yaganti <93136838+varshini777@users.noreply.github.com>
2025-06-27 09:19:38 -0400

    Add files via upload

---
+++ Two Pointer Approach Problems/Day_5_Problem.py
@@ -0,0 +1,37 @@
+# Upper Bound
+
+# Given a sorted array of nums and an integer x, write a program to find the upper bound of x. The upper bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than a given key i.e. x.
+
+# If no such index is found, return the size of the array.
+
+# Examples:
+# Input : n= 4, nums = [1,2,2,3], x = 2
+
+# Output:3
+
+# Explanation: Index 3 is the smallest index such that arr[3] > x.
+
+# Input : n = 5, nums = [3,5,8,15,19], x = 9
+
+# Output: 3
+# Explanation: Index 3 is the smallest index such that arr[3] > x.
+
+class Solution:
+    def upperBound(self, nums, x):
+        low=0
+        high=len(nums)-1
+        ans=0
+        while(low<=high):
+            mid=(low+high)//2
+            if(nums[mid]>x):
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+        
+# Time and Space Complexity:
+
+# Time: O(log n) – binary search on a sorted array
+
+# Space: O(1) – no extra space used
